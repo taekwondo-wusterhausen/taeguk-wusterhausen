3021f98253d94d490c1326fc4fe901440774102d
Jim Neuendorf <jim.neuendorf@gmx.de>
2023-03-28 16:03:30 +0200

    astro config base, gallery component, use mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config'
 
-import sitemap from "@astrojs/sitemap"
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jneuendorf.github.io',
+    base: '/taeguk-wusterhausen',
     experimental: {
         assets: true,
     },
-    integrations: [robotsTxt(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
 })
