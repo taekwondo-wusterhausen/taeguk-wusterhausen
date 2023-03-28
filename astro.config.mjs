bb124617970db5d234df30ef23f65919f9c95abc
Jim Neuendorf <jim.neuendorf@gmx.de>
2023-03-28 14:24:41 +0200

    add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jneuendorf.github.io',
     experimental: {
         assets: true,
     },
