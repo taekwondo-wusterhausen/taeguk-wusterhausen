ad431ff628970a0f13a8773b69e4711d37dacdae
Jim Neuendorf <jim.neuendorf@gmx.de>
2023-04-10 01:03:52 +0200

    enable yaml imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
-
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
+import yaml from '@rollup/plugin-yaml'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +13,7 @@ export default defineConfig({
         assets: true,
     },
     integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
+    vite: {
+        plugins: [yaml()],
+    },
 })
