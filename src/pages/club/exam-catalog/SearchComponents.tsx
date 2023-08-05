cf43cf3ff92807c7c919594d9f666bc375253260
Jim Neuendorf <jim.neuendorf@gmx.de>
2023-08-05 14:31:02 +0200

    taeguk-blue color from logo

--- src/pages/club/exam-catalog/SearchComponents.tsx
+++ src/pages/club/exam-catalog/SearchComponents.tsx
@@ -88,7 +88,7 @@ export function Button(
 ) {
     return (
         <button
-            className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="block text-white bg-taeguk-blue-500 hover:bg-taeguk-blue-600 focus:ring-4 focus:outline-none focus:ring-taeguk-blue-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-taeguk-blue-400 dark:hover:bg-taeguk-blue-500 dark:focus:ring-taeguk-blue-600"
             type="button"
             {...props}
         >
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
         './node_modules/flowbite/**/*.js',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // https://www.tailwindshades.com/#color=203.72881355932205%2C83.09859154929578%2C41.764705882352935&step-up=8&step-down=11&hue-shift=0&name=taeguk-blue&base-stop=5&v=1&overrides=e30%3D
+                'taeguk-blue': {
+                    DEFAULT: '#127DC3',
+                    50: '#58B4F0',
+                    100: '#4CAEEF',
+                    200: '#35A4EC',
+                    300: '#1E99EA',
+                    400: '#148CDA',
+                    500: '#127DC3',
+                    600: '#106EAC',
+                    700: '#0E5F94',
+                    800: '#0C507D',
+                    900: '#094166',
+                    950: '#083A5A'
+                },
+            },
+        },
         container: {
             center: true,
             padding: '2rem',
